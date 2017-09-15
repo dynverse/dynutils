2a675129a8d60a982fa3b2d9149312bc8a1d5887
Wouter Saelens <wouter.saelens@gmail.com>
2017-09-15 10:20:20 +0200

    added cell grouping

---
+++ R/cell_grouping.R
@@ -0,0 +1,7 @@
+#' Group cells to their highest milestone
+#' @param milestone_percentages milestone percentages dataframe
+#' @export
+get_cell_grouping <- function(milestone_percentages) {
+  milestone_percentages %>% group_by(cell_id) %>%
+    summarise(group_id=milestone_id[which.max(percentage)])
+}
