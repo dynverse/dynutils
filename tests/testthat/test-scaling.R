888f5b18807361304347ccd7228d027fe350921e
Robrecht Cannoodt <brechtus@gmail.com>
2017-10-16 14:10:15 +0200

    fix tests

--- tests/testthat/test-scaling.R
+++ tests/testthat/test-scaling.R
@@ -4,7 +4,8 @@ test_that("Testing scale_uniform", {
   # generate some random data
   num_samples <- 40
   num_dims <- 10
-  data <- matrix(runif(num_samples * num_dims), nrow = num_samples)
+  data <- matrix(runif(num_samples * num_dims), nrow = num_samples,
+                 dimnames = list(paste0("Sample", seq_len(num_samples)), paste0("Dim", seq_len(num_dims))))
 
   data_sc <- scale_uniform(data, center = 0, max_range = 1)
   ranges <- apply(data_sc, 2, range)
@@ -13,7 +14,8 @@ test_that("Testing scale_uniform", {
   expect_equal( nrow(data_sc), nrow(data) )
   expect_equal( ncol(data_sc), ncol(data) )
   expect_equal(max(ranges[2,] - ranges[1,]), 1)
-  expect_equal((ranges[1,] + ranges[2,]) / 2, rep(0, num_dims))
+  expect_equal((ranges[1,] + ranges[2,]) / 2, setNames(rep(0, num_dims), colnames(data)))
+  expect_equal(dimnames(data_sc), dimnames(data))
 
   # reapply
   data_sc2 <- apply_uniform_scale(data, attr(data_sc, "addend"), attr(data_sc, "multiplier"))
@@ -27,14 +29,16 @@ test_that("Testing scale_uniform", {
   expect_equal( nrow(data_sc), nrow(data) )
   expect_equal( ncol(data_sc), ncol(data) )
   expect_equal(max(ranges[2,] - ranges[1,]), 1000)
-  expect_equal(colMeans(ranges), rep(10, num_dims))
+  expect_equal(colMeans(ranges), setNames(rep(10, num_dims), colnames(data)))
 })
 
 test_that("Testing scale_uniform with edge case", {
   # generate some random data
   num_samples <- 1
   num_dims <- 10
-  data <- matrix(runif(num_samples * num_dims), nrow = num_samples)
+  data <- matrix(runif(num_samples * num_dims), nrow = num_samples,
+                 dimnames = list(paste0("Sample", seq_len(num_samples)), paste0("Dim", seq_len(num_dims))))
+
 
   data_sc <- scale_uniform(data, center = 0, max_range = 1)
   ranges <- apply(data_sc, 2, range)
@@ -43,7 +47,7 @@ test_that("Testing scale_uniform with edge case", {
   expect_equal( nrow(data_sc), nrow(data) )
   expect_equal( ncol(data_sc), ncol(data) )
   expect_equal(max(ranges[2,] - ranges[1,]), 0) # 0 because only 1 sample
-  expect_equal(ranges[1,] + ranges[2,], rep(0, num_dims))
+  expect_equal(ranges[1,] + ranges[2,], setNames(rep(0, num_dims), colnames(data)))
 
   # reapply
   data_sc2 <- apply_uniform_scale(data, attr(data_sc, "addend"), attr(data_sc, "multiplier"))
@@ -57,7 +61,7 @@ test_that("Testing scale_uniform with edge case", {
   expect_equal( nrow(data_sc), nrow(data) )
   expect_equal( ncol(data_sc), ncol(data) )
   expect_equal(max(ranges[2,] - ranges[1,]), 0) # 0 because only 1 sample
-  expect_equal(colMeans(ranges), rep(10, num_dims))
+  expect_equal(colMeans(ranges), setNames(rep(10, num_dims), colnames(data)))
 })
 
 test_that("Testing scale_uniform with another edge case", {
@@ -99,7 +103,8 @@ test_that("Testing scale_quantile", {
   # generate some random data
   num_samples <- 40
   num_dims <- 10
-  data <- matrix(runif(num_samples * num_dims), nrow = num_samples)
+  data <- matrix(runif(num_samples * num_dims), nrow = num_samples,
+                 dimnames = list(paste0("Sample", seq_len(num_samples)), paste0("Dim", seq_len(num_dims))))
 
   data_sc <- scale_quantile(data, outlier_cutoff = 0.05)
   ranges <- apply(data_sc, 2, range)
@@ -107,7 +112,8 @@ test_that("Testing scale_quantile", {
   expect_is( data_sc, "matrix" )
   expect_equal( nrow(data_sc), nrow(data) )
   expect_equal( ncol(data_sc), ncol(data) )
-  expect_equal( ranges, matrix(rep(c(0, 1), num_dims), ncol = num_dims) )
+  expect_equal( ranges, matrix(rep(c(0, 1), num_dims), ncol = num_dims, dimnames = list(NULL, colnames(data))) )
+  expect_equal(dimnames(data_sc), dimnames(data))
 
   # reapply
   data_sc2 <- apply_quantile_scale(data, attr(data_sc, "addend"), attr(data_sc, "multiplier"))
@@ -120,7 +126,7 @@ test_that("Testing scale_quantile", {
   expect_is( data_sc, "matrix" )
   expect_equal( nrow(data_sc), nrow(data) )
   expect_equal( ncol(data_sc), ncol(data) )
-  expect_equal( ranges, matrix(rep(c(0, 1), num_dims), ncol = num_dims) )
+  expect_equal( ranges, matrix(rep(c(0, 1), num_dims), ncol = num_dims, dimnames = list(NULL, colnames(data))) )
 })
 
 test_that("Testing scale_quantile with edge case", {
