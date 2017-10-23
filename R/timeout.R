7d0fc6a89f46cd49551f770cd16c9f62c2f31675
Robrecht Cannoodt <brechtus@gmail.com>
2017-10-23 10:42:04 +0200

    replace wait_or_kill with future implementation with eval_with_timeout with parallel implementation. works only on unix systems.

---
+++ R/timeout.R
@@ -0,0 +1,92 @@
+#' Wait for a handle to finish or kill it
+#'
+#' @param expr An R \code{\link[base]{expression}} to be evaluated.
+#' @param envir The environment in which the expression should be evaluated.
+#' @param timeout How long to wait before killing it.
+#' @param on_timeout A character specifying what action to take if a timeout event occurs. Must be one of: error, warning, silent.
+#'
+#' @importFrom parallel mcparallel mccollect
+#' @importFrom tools pskill
+#' @export
+#'
+#' @examples
+#' dry_run <- wait_or_kill({1}, wait_time = 5, function(x) x, 1)
+#' wait_or_kill(
+#'   expr = {
+#'     Sys.sleep(100) # really long function
+#'     data.frame(result = "finished", time = sleeper_time)
+#'   },
+#'   wait_time = 10,
+#'   cancel_output_fun = function(t) data.frame(result = "killed", time = t),
+#'   check_interval = 1,
+#'   verbose = TRUE
+#' )
+eval_with_timeout <- function(expr, envir = parent.frame(), timeout, on_timeout=c("error", "warning", "silent")) {
+  # substitute expression so it is not executed as soon it is used
+  expr <- substitute(expr)
+
+  # match on_timeout
+  on_timeout <- match.arg(on_timeout)
+
+  # execute expr in separate fork
+  myfork <- parallel::mcparallel({
+    eval(expr, envir = envir)
+  }, silent = FALSE)
+
+  # wait max n seconds for a result.
+  myresult <- parallel::mccollect(myfork, wait = FALSE, timeout = timeout)
+  # kill fork after collect has returned
+  tools::pskill(myfork$pid, tools::SIGKILL)
+  tools::pskill(-1 * myfork$pid, tools::SIGKILL)
+
+  # clean up:
+  parallel::mccollect(myfork, wait = FALSE)
+
+  # timeout?
+  if (is.null(myresult)) {
+    if (on_timeout == "error") {
+      stop("reached elapsed time limit")
+    } else if (on_timeout == "warning") {
+      warning("reached elapsed time limit")
+    } else if (on_timeout == "silent") {
+      myresult <- NA
+    }
+  }
+
+  # move this to distinguish between timeout and NULL returns
+  myresult <- myresult[[1]]
+
+  # send the buffered response
+  return(myresult)
+}
+
+# old version
+# wait_or_kill <- function(expr, wait_time, cancel_output_fun, check_interval = 1, verbose = FALSE, ...) {
+#   expr <- substitute(expr)
+#   envir <- parent.frame()
+#
+#   requireNamespace("future")
+#   start_time <- Sys.time()
+#
+#   future_handle <- future(
+#     { expr },
+#     evaluator = plan("multisession"),
+#     substitute = FALSE,
+#     envir = envir,
+#     ...
+#   )
+#
+#   time_waited <- 0
+#   while (!resolved(future_handle) && time_waited < wait_time) {
+#     Sys.sleep(check_interval)
+#     time_waited <- as.numeric(difftime(Sys.time(), start_time, units = "secs"))
+#     if (verbose) cat("Time waited: ", time_waited, "\n", sep = "")
+#   }
+#
+#   if (resolved(future_handle)) {
+#     value(future_handle)
+#   } else {
+#     future:::ClusterRegistry("stop")
+#     cancel_output_fun(time_waited)
+#   }
+# }
---
+++ tests/testthat/test-timeout.R
@@ -0,0 +1,69 @@
+context("Timeout helper")
+
+test_that("Timeouts work in simple use cases", {
+  # expect error on timeout
+  expect_error(eval_with_timeout({Sys.sleep(10); 1}, timeout = 3))
+
+  # expect correct result when no timeout
+  expect_equal(eval_with_timeout({Sys.sleep(1); 5.5}, timeout = 5), 5.5)
+})
+
+test_that("Timeouts work in parallel usecases", {
+  sleeper_times <- sample(c(runif(10, .5, 3), runif(10, 7, 10)))
+  wait_time <- 5
+
+  parallelMap::parallelStartMulticore(cpus = 2, show.info = TRUE)
+  outp <- parallelMap::parallelMap(sleeper_times, fun = function(t) {
+    eval_with_timeout(
+      {
+        Sys.sleep(t)
+        t
+      },
+      timeout = wait_time,
+      on_timeout = "silent"
+    )
+  })
+  parallelMap::parallelStop()
+
+  outp <- unlist(outp, recursive = FALSE)
+
+  expected_result <- ifelse(sleeper_times <= wait_time, sleeper_times, NA)
+  are_same <- abs(outp - expected_result) < 1e-5 | (is.na(outp) & is.na(expected_result))
+  expect_true(all(are_same))
+})
+
+
+test_that("Packages loaded in forked environments do not override the global", {
+  sess_info_before <- utils::sessionInfo()
+
+  z <- eval_with_timeout(
+    {
+      library(dplyr)
+      library(magrittr)
+      library(methods)
+      library(purrr)
+      library(testthat)
+      library(tibble)
+      library(tidyr)
+      library(reshape2)
+      library(igraph)
+      library(transport)
+      10
+    },
+    timeout = 20,
+    on_timeout = "silent"
+  )
+  sess_info_after <- utils::sessionInfo()
+
+  expect_equal(z, 10)
+
+  expect_equal(sess_info_after$otherPkgs, sess_info_before$otherPkgs)
+})
+
+test_that("Timeouts work in system calls", {
+  # expect error on timeout
+  expect_error(eval_with_timeout({system("sleep 10"); 1}, timeout = 3))
+
+  # expect correct result when no timeout
+  expect_equal(eval_with_timeout({system("sleep 1"); 5.5}, timeout = 5), 5.5)
+})
