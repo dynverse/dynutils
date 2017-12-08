13ff8df6b914d0167339f46416f1f1b7c921dd54
Robrecht Cannoodt <brechtus@gmail.com>
2017-12-08 11:24:09 +0100

    fix random time string test

--- tests/testthat/test-random_time_string.R
+++ tests/testthat/test-random_time_string.R
@@ -1,8 +1,9 @@
 context("Random time string helper")
 
 test_that("Generating random strings", {
-  random_strings <- sapply(seq_len(10), function(x) random_time_string())
+  random_strings <- sapply(seq_len(1000), function(x) random_time_string())
   expect_equal(length(unique(random_strings)), length(random_strings))
 
-  expect_true(all(grepl("\\d\\d\\d\\d_\\d\\d_\\d\\d_\\d\\d_\\d\\d_\\d\\d__\\d+", random_strings)))
+  random_strings <- sapply(seq_len(1000), function(x) random_time_string("somestring"))
+  expect_equal(length(unique(random_strings)), length(random_strings))
 })
