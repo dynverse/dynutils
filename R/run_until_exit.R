13afb44f07460ada07076f32a911a9bf9fdee74c
Wouter Saelens <wouter.saelens@gmail.com>
2018-01-24 10:50:37 +0100

    fix run_until_exit

--- R/run_until_exit.R
+++ R/run_until_exit.R
@@ -3,15 +3,22 @@
 #'
 #' @param command Character scalar, the command to run.
 #' @export
-run_until_exit <- function(command) {
+run_until_exit <- function(commands, bash=TRUE) {
+  if(bash) {
+    command <- paste0("/bin/bash -c ", "'", glue::collapse(commands, ";"), "'")
+  } else {
+    command <- glue::collapse(commands, "\n")
+  }
+
   cmd <- processx::process$new(commandline=command, stdout = "|", stderr = "|")
   tryCatch(
     {
-      cmd$poll_io(9999999) # wait for process to be finished
+      cmd$wait() # wait for process to be finished
     },
     finally={
-      cmd$kill()
-      stop("Command was terminated", call.=FALSE)
+      if (cmd$is_alive()) {
+        cmd$kill()
+      }
     }
   )
 
@@ -19,6 +26,7 @@ run_until_exit <- function(command) {
   error <- cmd$read_all_error_lines()
 
   if (cmd$get_exit_status() != 0) {
+    print(cmd$get_exit_status())
     stop(glue::collapse(c("Error running command: ", crayon::white$bold(command), "-- output --", crayon::blue(output), "-- error --", crayon::red(error)), "\n"), call.=FALSE)
   }
 
