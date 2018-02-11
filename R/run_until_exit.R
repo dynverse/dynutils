d9e9101d871ea7a4695ade184868ed9c5da0ef46
Robrecht Cannoodt <robrecht.cannoodt@ugent.be>
2018-02-11 19:42:41 +0100

    reformat run_until_exit

--- R/run_until_exit.R
+++ R/run_until_exit.R
@@ -4,34 +4,43 @@
 #' @param commands Character scalar, the command to run.
 #' @param bash Whether to run the command within a bash shell
 #' @export
-run_until_exit <- function(commands, bash=TRUE) {
-  if(bash) {
+run_until_exit <- function(commands, bash = TRUE) {
+  if (bash) {
     command <- paste0("/bin/bash -c ", "'", glue::collapse(commands, ";"), "'")
   } else {
     command <- glue::collapse(commands, "\n")
   }
 
-  cmd <- processx::process$new(commandline=command, stdout = "|", stderr = "|")
-  tryCatch(
-    {
-      cmd$wait() # wait for process to be finished
-    },
-    finally={
-      if (cmd$is_alive()) {
-        cmd$kill()
-      }
+  cmd <- processx::process$new(commandline = command, stdout = "|", stderr = "|")
+  tryCatch({
+    cmd$wait() # wait for process to be finished
+  },
+  finally = {
+    if (cmd$is_alive()) {
+      cmd$kill()
     }
-  )
+  })
 
   output <- cmd$read_all_output_lines()
   error <- cmd$read_all_error_lines()
 
   if (cmd$get_exit_status() != 0) {
     print(cmd$get_exit_status())
-    stop(glue::collapse(c("Error running command: ", crayon::white$bold(command), "-- output --", crayon::blue(output), "-- error --", crayon::red(error)), "\n"), call.=FALSE)
+    stop(
+      glue::collapse(
+        c(
+          "Error running command: ", crayon::white$bold(command),
+          "-- output --", crayon::blue(output),
+          "-- error --", crayon::red(error)
+        ),
+        "\n"
+      ),
+      call. = FALSE
+    )
   }
 
   tibble::lst(
-    output, error
+    output,
+    error
   )
 }
