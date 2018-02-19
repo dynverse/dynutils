c4316feb3e24749832a051674c71dd7563a4a6a8
Robrecht Cannoodt <brechtus@gmail.com>
2018-02-19 16:45:35 +0100

    add trajectory segments to dimred

--- R/wrap_add_dimred_to_wrapper.R
+++ R/wrap_add_dimred_to_wrapper.R
@@ -2,7 +2,8 @@
 #'
 #' @param data_wrapper A data wrapper to extend upon.
 #' @param dimred The dimensionality reduction matrix.
-#' @param dimred_milestone An optional dimensionality reduction of the milestones.
+#' @param dimred_milestones An optional dimensionality reduction of the milestones.
+#' @param dimred_trajectory_segments An optional dimensionality reduction of the trajectory segments.
 #' @param ... extra information to be stored in the wrapper
 #'
 #' @export
@@ -11,7 +12,8 @@
 add_dimred_to_wrapper <- function(
   data_wrapper,
   dimred,
-  dimred_milestone = NULL,
+  dimred_milestones = NULL,
+  dimred_trajectory_segments = NULL,
   ...
 ) {
   testthat::expect_is(data_wrapper, "dynutils::data_wrapper")
@@ -21,12 +23,22 @@ add_dimred_to_wrapper <- function(
   testthat::expect_is(dimred, "matrix")
   testthat::expect_equal(rownames(dimred), cell_ids)
 
-  if (!is.null(dimred_milestone)) {
+  if (!is.null(dimred_milestones)) {
     testthat::expect_true(is_wrapper_with_trajectory(data_wrapper))
 
     milestone_ids <- data_wrapper$milestone_ids
-    testthat::expect_is(dimred_milestone, "matrix")
-    testthat::expect_equal(rownames(dimred_milestone), milestone_ids)
+    testthat::expect_is(dimred_milestones, "matrix")
+    testthat::expect_equal(rownames(dimred_milestones), milestone_ids)
+    testthat::expect_equal(colnames(dimred_milestones), colnames(dimred))
+  }
+
+  if (!is.null(dimred_trajectory_segments)) {
+    testthat::expect_is(dimred_trajectory_segments, "matrix")
+    expected_colnames <- c(
+      paste0("from_", colnames(dimred)),
+      paste0("to_", colnames(dimred))
+    )
+    testthat::expect_equal(colnames(dimred_trajectory_segments), expected_colnames)
   }
 
   # create output structure
@@ -34,7 +46,8 @@ add_dimred_to_wrapper <- function(
     data_wrapper,
     list(
       dimred = dimred,
-      dimred_milestone = dimred_milestone,
+      dimred_milestones = dimred_milestones,
+      dimred_trajectory_segments = dimred_trajectory_segments,
       ...
     ))
   class(out) <- c("dynutils::with_dimred", class(data_wrapper))
