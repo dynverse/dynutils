294af933f8f9e3bc1ba2f20757891a1b67a5be23
Robrecht Cannoodt <brechtus@gmail.com>
2018-02-20 17:02:34 +0100

    add manhattan distance function

--- R/package.R
+++ R/package.R
@@ -7,6 +7,8 @@
 #' @importFrom purrr %>% map map_df map_chr map_lgl map_int map_dbl keep set_names
 #' @importFrom magrittr %<>% %$% set_rownames set_colnames
 #'
+#' @useDynLib dynutils
+#'
 #' @docType package
 #' @name dynutils
 NULL
