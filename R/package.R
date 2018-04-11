82c1b6a1b75629d95fd4b94fe757d073d6a0f251
Robrecht Cannoodt <brechtus@gmail.com>
2018-04-11 17:59:04 +0200

    add stringr dependency

--- R/package.R
+++ R/package.R
@@ -4,6 +4,7 @@
 #' @import tidyr
 #' @import methods
 #' @import tibble
+#' @import stringr
 #' @importFrom purrr %>% map map_df map_chr map_lgl map_int map_dbl keep set_names
 #' @importFrom magrittr %<>% %$% set_rownames set_colnames
 #'
