159ad16eed1f79201f82867c4401b286b9ffb072
Robrecht Cannoodt <brechtus@gmail.com>
2018-04-12 10:56:16 +0200

    inherit documentation from glue::glue

--- R/nice_glue.R
+++ R/nice_glue.R
@@ -2,15 +2,7 @@
 #'
 #' Uses \code{\link[glue]{glue}}, but removes the class from the output.
 #'
-#' @param ... \[`expressions`]\cr Expressions string(s) to format, multiple inputs are concatenated together before formatting.
-#' @param .sep \[`character(1)`: \sQuote{""}]\cr Separator used to separate elements.
-#' @param .envir \[`environment`: `parent.frame()`]\cr Environment to evaluate each expression in. Expressions are
-#'   evaluated from left to right. If `.x` is an environment, the expressions are
-#'   evaluated in that environment and `.envir` is ignored.
-#' @param .open \[`character(1)`: \sQuote{\\\{}]\cr The opening delimiter. Doubling the
-#'   full delimiter escapes it.
-#' @param .close \[`character(1)`: \sQuote{\\\}}]\cr The closing delimiter. Doubling the
-#'   full delimiter escapes it.
+#' @inheritParams glue::glue
 #'
 #' @importFrom glue glue
 #'
