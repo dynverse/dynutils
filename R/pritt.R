95b42e8ebe93eaf1a1595a30376d200014c9521b
Robrecht Cannoodt <brechtus@gmail.com>
2018-04-12 11:16:09 +0200

    fix pritt and documentation

--- R/pritt.R
+++ R/pritt.R
@@ -2,13 +2,21 @@
 #'
 #' Uses \code{\link[glue]{glue}}, but removes the class from the output.
 #'
-#' @param ... Parameters passed to \code{\link[glue]{glue}}.
+#' @param ... [\code{expressions}]\cr Expressions string(s) to format, multiple inputs are concatenated together before formatting.
+#' @param .sep [\code{character(1)}: \sQuote{""}]\cr Separator used to separate elements.
+#' @param .envir [\code{environment}: \code{parent.frame()}]\cr Environment to evaluate each expression in. Expressions are
+#'   evaluated from left to right. If \code{.x} is an environment, the expressions are
+#'   evaluated in that environment and \code{.envir} is ignored.
+#' @param .open [\code{character(1)}: \sQuote{\\\{}]\cr The opening delimiter. Doubling the
+#'   full delimiter escapes it.
+#' @param .close [\code{character(1)}: \sQuote{\\\}}]\cr The closing delimiter. Doubling the
+#'   full delimiter escapes it.
 #'
 #' @importFrom glue glue
 #'
 #' @seealso \code{\link[glue]{glue}}
 #'
 #' @export
-pritt <- function(...) {
-  as.character(glue::glue(...))
+pritt <- function(..., .sep = "", .envir = parent.frame(), .open = "{", .close = "}") {
+  as.character(glue::glue(..., .sep = .sep, .envir = .envir, .open = .open, .close = .close))
 }
