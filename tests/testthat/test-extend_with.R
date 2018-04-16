a8430e4d7ba4004a2282c801e3aca00414c47d26
Robrecht Cannoodt <brechtus@gmail.com>
2018-04-16 15:47:53 +0200

    test ...

--- tests/testthat/test-extend_with.R
+++ tests/testthat/test-extend_with.R
@@ -15,4 +15,18 @@ test_that("Test extend_with", {
   testthat::expect_equal(new_list$a, 1)
   testthat::expect_equal(new_list$b, "3")
   testthat::expect_equal(new_list$c, list(1))
+
+  new_list <- orig_list %>% extend_with(
+    "extension2",
+    b = list(2),
+    c = "10"
+  )
+
+  testthat::expect_is(new_list, "extension2")
+  testthat::expect_is(new_list, "list")
+
+  testthat::expect_equal(length(new_list), 3)
+  testthat::expect_equal(new_list$a, 1)
+  testthat::expect_equal(new_list$b, list(2))
+  testthat::expect_equal(new_list$c, "10")
 })
