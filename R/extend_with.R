3525ed6686ff543d8d1052ec76c8d5c05afd36dd
Robrecht Cannoodt <brechtus@gmail.com>
2018-04-16 15:36:28 +0200

    support ... in extend_with

--- R/extend_with.R
+++ R/extend_with.R
@@ -2,17 +2,27 @@
 #'
 #' @param object A list
 #' @param class_name A class name to add
-#' @param extension Extra information in the list
+#' @param ... Extra information in the list
+#'
+#' @importFrom testthat expect_is expect_named
 #'
 #' @export
 extend_with <- function(
   object,
   class_name,
-  extension
+  ...
 ) {
   testthat::expect_is(object, "list")
   testthat::expect_is(class_name, "character")
+
+  extension <- list(...)
+
+  if (is.null(names(extension)) && length(extension) == 1 && "list" %in% class(extension[[1]])) {
+    extension <- extension[[1]]
+  }
+
   testthat::expect_is(extension, "list")
+  testthat::expect_named(extension)
 
   object[names(extension)] <- extension
 
