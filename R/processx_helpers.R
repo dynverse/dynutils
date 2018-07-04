9f825ee641339134ff931b1985c52c76e61089a7
Robrecht Cannoodt <brechtus@gmail.com>
2018-07-04 15:21:35 +0200

    defenestrate processx helper function

--- R/processx_helpers.R
+++
@@ -1,9 +0,0 @@
-#' Processx callback function for printing stdout or stderr
-#'
-#' @param x The stdout/stderr
-#' @param proc The processx process
-#'
-#' @export
-print_processx <- function(x, proc) {
-  cat(x)
-}
