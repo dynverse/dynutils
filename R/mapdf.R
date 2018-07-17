9fa9411719ef63d860d743ac9a277acf4e974f23
Robrecht Cannoodt <rcannood@gmail.com>
2018-07-17 15:45:01 +0200

    remove mapdf_if and mapdf_at

--- R/mapdf.R
+++ R/mapdf.R
@@ -14,7 +14,7 @@ mapdf_fun <- function(purrrfun) {
 #' The mapdf functions transform their input by applying a function to each row of a data frame and returning a vector the same length as the input.
 #' These functions work a lot like purrr's \code{\link[purrr]{map}} functions.
 #'
-#'  * \code{mapdf()}, \code{mapdf_if()} and \code{mapdf_at()} always return a list.
+#'  * \code{mapdf()} always returns a list.
 #'
 #'  * \code{mapdf_lgl()}, \code{mapdf_int()}, \code{mapdf_dbl()} and \code{mapdf_chr()} return vectors of the corresponding type (or die trying).
 #'
@@ -87,23 +87,3 @@ mapdf_dfc <- mapdf_fun(map_dfc)
 #' @export
 #' @rdname mapdf
 walkdf <- mapdf_fun(walk)
-
-#' @export
-#' @rdname mapdf
-mapdf_if <- function(.x, .p, .f, ...) {
-  .f <- purrr::as_mapper(.f, ...)
-  map_if(seq_len(nrow(.x)), .p = .p, function(row_ix) {
-    row <- extract_row_to_list(.x, row_ix)
-    .f(row, ...)
-  })
-}
-
-#' @export
-#' @rdname mapdf
-mapdf_at <- function(.x, .at, .f, ...) {
-  .f <- purrr::as_mapper(.f, ...)
-  map_at(seq_len(nrow(.x)), .at = .at, function(row_ix) {
-    row <- extract_row_to_list(.x, row_ix)
-    .f(row, ...)
-  })
-}
