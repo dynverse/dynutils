89eca71ff35bfa2051498e285c5e07994cdf0c32
Robrecht Cannoodt <rcannood@gmail.com>
2019-03-21 16:59:47 +0100

    fix news test

--- tests/testthat/test-news.R
+++ tests/testthat/test-news.R
@@ -45,9 +45,9 @@ test_that("update_news works", {
   expect_false(any(grepl("testing 0.0.1", recent_news)))
 
   file.remove(news, news_md)
-  expect_error(find_news(path = path), regexp = "does not have a NEWS.md")
+  expect_error(find_news(path = dir_loc), regexp = "does not have a NEWS.md")
 
   file.remove(description)
-  expect_error(detect_package_folder(path = path), regexp = "Could not find DESCRIPTION file")
+  expect_error(detect_package_folder(path = dir_loc), regexp = "Could not find DESCRIPTION file")
 })
 
