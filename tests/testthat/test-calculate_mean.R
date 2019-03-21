1d235111422b4578f0a33a903f80508de27da7c2
Robrecht Cannoodt <rcannood@gmail.com>
2019-03-21 15:01:43 +0100

    update test

--- tests/testthat/test-calculate_mean.R
+++ tests/testthat/test-calculate_mean.R
@@ -1,6 +1,6 @@
 context("Means")
 
-test_that(paste0("calculate_means"), {
+test_that(paste0("calculate_mean"), {
   testthat::expect_equal(calculate_arithmetic_mean(0.5, 0.6, 0.7), 0.6)
   testthat::expect_equal(calculate_harmonic_mean(0.5, 1, 1), 0.75)
   testthat::expect_equal(calculate_geometric_mean(0.001, 1, 1), 0.1)
@@ -12,4 +12,14 @@ test_that(paste0("calculate_means"), {
   testthat::expect_true(length(calculate_arithmetic_mean(tibble::tibble(x = numeric(), y = numeric()), weights = c(x = 0, y = 1))) == 0)
   testthat::expect_true(length(calculate_harmonic_mean(tibble::tibble(x = numeric(), y = numeric()), weights = c(x = 0, y = 1))) == 0)
   testthat::expect_true(length(calculate_geometric_mean(tibble::tibble(x = numeric(), y = numeric()), weights = c(x = 0, y = 1))) == 0)
+
+  testthat::expect_equal(calculate_mean(0.1, 0.4, 0.4, weights = c(1, 2, 5), method = "arithmetic"), 0.3625)
+  testthat::expect_equal(calculate_mean(0.5, 1, 1, weights = c(1, 2, 4)), method = "harmonic", 0.875)
+  testthat::expect_equal(calculate_mean(0.001, 1, 1, weights = c(1, 2, 0)), method = "geometric", 0.1)
+
+  testthat::expect_equal(calculate_arithmetic_mean(c(1, 2, 3), c(4, 5, 6), weights = c(2, 1)), c(2, 3, 4))
+  exp <- 3 / c(1 / .2 + 1 / .2 + 1 / .3, 1 / .2 + 1 / .2 + 1 / .4, 1 / .3 + 1 / .3 + 1 / .6)
+  testthat::expect_equal(calculate_harmonic_mean(c(0.2, 0.2, 0.3), c(0.3, 0.4, 0.6), weights = c(2, 1)), exp)
+  expr <- c(.2 * .2 * .3, .2 * .2 * .4, .3 * .3 * .6)^(1 / 3)
+  testthat::expect_equal(calculate_harmonic_mean(c(0.2, 0.2, 0.3), c(0.3, 0.4, 0.6), weights = c(2, 1)), exp)
 })
