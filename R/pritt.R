4a6d61709243ac2d22969dbca3743f1cfd6a61f8
Robrecht Cannoodt <rcannood@gmail.com>
2019-04-11 07:55:41 +0200

    fix docs

--- R/pritt.R
+++ R/pritt.R
@@ -4,12 +4,12 @@
 #'
 #' @param ... `expressions`\cr Expressions string(s) to format, multiple inputs are concatenated together before formatting.
 #' @param .sep `character(1): \sQuote{""}`\cr Separator used to separate elements.
-#' @param .envir `environment: parent.frame()`\cr Environment to evaluate each expression in. Expressions are
+#' @param .envir \[`environment`: `parent.frame()`]\cr Environment to evaluate each expression in. Expressions are
 #'   evaluated from left to right. If `.x` is an environment, the expressions are
 #'   evaluated in that environment and `.envir` is ignored.
-#' @param .open `character(1): \sQuote{\\\{}`\cr The opening delimiter. Doubling the
+#' @param .open \[`character(1)`: \sQuote{\\\{}]\cr The opening delimiter. Doubling the
 #'   full delimiter escapes it.
-#' @param .close `character(1): \sQuote{\\\}}`\cr The closing delimiter. Doubling the
+#' @param .close \[`character(1)`: \sQuote{\\\}}]\cr The closing delimiter. Doubling the
 #'   full delimiter escapes it.
 #'
 #' @importFrom glue glue
